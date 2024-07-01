eb4fbef5028cc9e1c876fb9a110c36881c019d50
btrindadefm <btrindade172@gmail>
2024-07-01 01:06:45 -0300

    feat: calculadora feita com react

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import './index.css'
+import { useState } from 'react';
+
+function App() {
+
+  const [resultado, setResultado] = useState("")
+
+  function acao (e) {
+    // Será adicionado o valor do botão que foi clicado na variável resultado
+    // e de event. O evento nesse caso é o click
+    setResultado(resultado + e.target.innerText)
+  }
+
+  function calcular () {
+    // Try-Catch necessário 
+    try {
+      // Método eval() para realizar contas matemáticas 
+      // Método String() para transformar em String
+      setResultado(eval(String(resultado)));
+    } catch (e) {
+      setResultado("Erro");
+    }
+  }
+
+  function resetar () {
+    // Transforma a variável setResultado em uma String vazia
+    setResultado("");
+  }
+
+  return (
+    <>
+      <section id="calculadora" className='container'>
+        <input type="text" value={resultado} id='resultado' readOnly/>
+        <div id="botoes">
+          <button onClick={acao}>7</button>
+          <button onClick={acao}>8</button>
+          <button onClick={acao}>9</button>
+          <button className='operadores' onClick={acao}>+</button>
+          <button onClick={acao}>4</button>
+          <button onClick={acao}>5</button>
+          <button onClick={acao}>6</button>
+          <button className='operadores' onClick={acao}>-</button>
+          <button onClick={acao}>1</button>
+          <button onClick={acao}>2</button>
+          <button onClick={acao}>3</button>
+          <button className='operadores' onClick={acao}>*</button>
+          <button onClick={acao}>0</button>
+          <button onClick={acao}>.</button>
+          <button onClick={resetar}>C</button>
+          <button className='operadores' onClick={acao}>/</button>
+          <button className='calcular' onClick={calcular}>=</button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
