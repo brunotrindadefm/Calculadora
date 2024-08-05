3dac391f32249fe788709c92a00dcfe59c33c1fd
btrindadefm <btrindade172@gmail>
2024-08-04 23:37:16 -0300

    removendo arquivos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './index.css'
+import './index.scss'
 import Calculadora from './Calculadora/Calculadora'
 
 function App() {
--- src/Calculadora/Calculadora.jsx
+++ src/Calculadora/Calculadora.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './Calculadora.css'
+import './Calculadora.scss'
 
 const Calculadora = () => {
 
